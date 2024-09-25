d4a6005a750a463818f8d9dea82ae546a2fc50f6
Breno <brenomaia.dev@hotmail.com>
2024-09-25 17:59:46 -0300

    implementação do banco de dados

--- dist/db.js
+++ dist/db.js
@@ -1,9 +1,19 @@
 // db.ts
 import { Sequelize } from 'sequelize';
-const db = new Sequelize('bcfionode', 'root', 'supre123', {
-    host: 'localhost',
-    port: 3300,
+import dotenv from 'dotenv';
+dotenv.config();
+const db = new Sequelize(process.env.DB_NAME || 'prova', process.env.DB_USER || 'root', process.env.DB_PASSWORD || '123456', {
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306, // Verifique se o MySQL está rodando na porta correta
     dialect: 'mysql',
-    logging: false,
+    logging: false, // Desativa logs de queries SQL
+});
+// Teste de conexão
+db.authenticate()
+    .then(() => {
+    console.log('Conexão estabelecida com sucesso.');
+})
+    .catch((error) => {
+    console.error('Não foi possível conectar ao banco de dados:', error);
 });
 export default db;
